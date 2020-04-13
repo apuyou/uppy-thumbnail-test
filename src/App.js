066e31a75b99c667d9e0b710eef924ecfcc9221f
Arthur Puyou <arthur@puyou.fr>
2020-04-13 21:12:40 +0200

    Add Uppy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Uppy from '@uppy/core';
+import XHRUpload from '@uppy/xhr-upload';
+import Dashboard from '@uppy/dashboard';
+import '@uppy/core/dist/style.css';
+import '@uppy/dashboard/dist/style.css';
+
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const uppy = Uppy()
+      .use(Dashboard, {
+        inline: true,
+        target: '#drag-drop-area',
+      })
+      .use(XHRUpload, { endpoint: 'https://api2.transloadit.com' });
+
+    uppy.on('complete', (result) => {
+      console.log(
+        'Upload complete! We’ve uploaded these files:',
+        result.successful
+      );
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="drag-drop-area"></div>
       </header>
     </div>
   );
